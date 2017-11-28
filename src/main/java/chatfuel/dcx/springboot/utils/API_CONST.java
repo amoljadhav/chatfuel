7f94841d70a4c891de7a194c8947fda67cc049ed
Amol Jadhav <jadhav.amol@gmail.com>
2017-11-28 09:59:52 +0530

    Update API_CONST.java

--- src/main/java/chatfuel/dcx/springboot/utils/API_CONST.java
+++ src/main/java/chatfuel/dcx/springboot/utils/API_CONST.java
@@ -39,22 +39,20 @@ public class API_CONST {
 	public static final String CLAIMS = "claims.";
 	public static final String PLEASE_CHOOSE = " Please choose one.";
 
-	public static final List<String> ELEMENTS_TITLE = Arrays.asList("AXA SmartCare Executive", 
-			"AXA SmartCare Executive Plus",
-			"AXA SmartCare Optimum", 
-			"International Exclusive",
-			"AXA SmartCare Cancer",
-			"AXA SmartCare Senior",
-			"AXA HappyCare");
+	public static final List<String> ELEMENTS_TITLE = Arrays.asList("AXA SmartCare Executive",                        						"AXA SmartCare Executive Plus",
+					"AXA SmartCare Optimum",
+                        "International Exclusive",
+                        "AXA SmartCare Cancer",
+                        "AXA SmartCare Senior",
+                        "AXA HappyCare");
 
 	public static final List<String> ELEMENTS_URL = Arrays.asList("https://thailandgi.cdn.axa-contento-118412.eu/thailandgi/686d4b1486d132bdb296a46f9d330069a3778d78_thumbnail.jpg",
-			"https://thailandgi.cdn.axa-contento-118412.eu/thailandgi/7aac3974dd72ff16e10db496af8ec50ebc43bfd8_thumbnail.jpg",
-			"https://thailandgi.cdn.axa-contento-118412.eu/thailandgi/725123f1c39e531750cff5a99194bcc3f7994181_thumbnail.jpg",
-			"https://thailandgi.cdn.axa-contento-118412.eu/thailandgi/a18a824f2937246d68d8b351261c5d4b7550668d_thumbnail.jpg",
-			"https://thailandgi.cdn.axa-contento-118412.eu/thailandgi/31c19f20c777106660a8759b20a6c75734a8a274_thumb-smartcare-cancer.jpg",
-			"https://thailandgi.cdn.axa-contento-118412.eu/thailandgi/8b13babd463c54f88d8ffd24fd4e4165b1e295f8_thumb-senior-health-sookjai.jpg",
-			"https://thailandgi.cdn.axa-contento-118412.eu/thailandgi/79e3cfaca0451629a670460437138502b28d00ca_bnr-product-care.jpg"
-			);
+                        "https://thailandgi.cdn.axa-contento-118412.eu/thailandgi/7aac3974dd72ff16e10db496af8ec50ebc43bfd8_thumbnail.jpg",
+                        "https://thailandgi.cdn.axa-contento-118412.eu/thailandgi/725123f1c39e531750cff5a99194bcc3f7994181_thumbnail.jpg",
+                        "https://thailandgi.cdn.axa-contento-118412.eu/thailandgi/a18a824f2937246d68d8b351261c5d4b7550668d_thumbnail.jpg",
+                        "https://thailandgi.cdn.axa-contento-118412.eu/thailandgi/31c19f20c777106660a8759b20a6c75734a8a274_thumb-smartcare-cancer.jpg",
+                        "https://thailandgi.cdn.axa-contento-118412.eu/thailandgi/8b13babd463c54f88d8ffd24fd4e4165b1e295f8_thumb-senior-health-sookjai.jpg",
+                        "https://thailandgi.cdn.axa-contento-118412.eu/thailandgi/79e3cfaca0451629a670460437138502b28d00ca_bnr-product-care.jpg"	);
 	
 	public static final String VIEW_POLICY = "View Policy";
 
